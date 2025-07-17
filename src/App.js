b05bb265dd1ba489fa4b8d9c7b1183337b83a297
ShiZhe <tanshizhek@gmail.com>
2025-07-17 11:34:47 +0800

    feat: implement basic home & about me

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import './index.css';
 
 export default function App() {
 
